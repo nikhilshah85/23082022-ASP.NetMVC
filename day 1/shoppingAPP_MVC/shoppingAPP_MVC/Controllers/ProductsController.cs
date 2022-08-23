af7f7753285e1e764e37270c29bf56833da0aa29
Nikhil <nikhil.shah2008@gmail.com>
2022-08-23 10:55:46 +0530

    Day 1

---
+++ day 1/shoppingAPP_MVC/shoppingAPP_MVC/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shoppingAPP_MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+       public IActionResult Productlist()
+        {            
+            return View();
+        }
+
+        public IActionResult OrdersList()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
