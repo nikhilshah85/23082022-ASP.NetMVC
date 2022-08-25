aa9df55c5469623eba2cc6f82ea407641a5632be
Nikhil <nikhil.shah2008@gmail.com>
2022-08-25 10:34:13 +0530

    Day 3 CRUD

---
+++ day 3/day 3 - In Memory CRUD/shoppingAPP/shoppingAPP/Controllers/ProductsController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shoppingAPP.Models;
+namespace shoppingAPP.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        //this will be replaced with DI later on
+        //createing a new object is like a crime now
+        ProductsModel pObj = new ProductsModel();
+        public IActionResult ShowAllProducts()
+        {
+            ViewBag.allProducts = pObj.GetAllProducts();
+            return View();
+        }
+
+        public IActionResult SearchProductById()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchProductById(int productId)
+        {
+          
+
+                ViewBag.product = pObj.GetProductById(productId);
+            
+          
+            return View();
+        }
+
+
+        public IActionResult SearchProductByCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SearchProductByCategory(string productCategory)
+        {
+            ViewBag.productByCategory = pObj.GetAllProductsByCategory(productCategory);
+            return View();
+        }
+
+
+        public IActionResult AddProduct()
+        {
+            ViewBag.addResult = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(ProductsModel newProduct)
+        {
+             ViewBag.addResult =  pObj.AddProduct(newProduct);
+           return  RedirectToAction("ShowAllProducts");
+        }
+
+        public IActionResult DeleteProduct()
+        {
+            ViewBag.deleteResult = "";
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult DeleteProduct(int productId)
+        {
+            ViewBag.deleteResult = pObj.DeleteProduct(productId);
+            return View();
+        }
+
+        public IActionResult UpdateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(ProductsModel update)
+        {
+            ViewBag.udpateResult = pObj.UpdateProduct(update);
+            return View();
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
---
+++ day 3/day 3 - In Memory CRUD/shoppingAPP/shoppingAPP/Models/ProductsModel.cs
@@ -0,0 +1,77 @@
+﻿namespace shoppingAPP.Models
+{
+    public class ProductsModel
+    {
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public double pPrice { get; set; }
+        public string pCategory { get; set; }
+        public bool pIsInStock { get; set; }
+
+        static List<ProductsModel> pList = new List<ProductsModel>()
+        {
+            new ProductsModel(){ pId=101, pName="Pepsi", pCategory="Cold-Drink", pIsInStock=true, pPrice=50},
+            new ProductsModel(){ pId=102, pName="Coke", pCategory="Cold-Drink", pIsInStock=true, pPrice=50},
+            new ProductsModel(){ pId=103, pName="IPhone", pCategory="Electronics", pIsInStock=false, pPrice=50000},
+
+        };
+
+        public List<ProductsModel> GetAllProducts()
+        {
+            return pList;
+        }
+
+        public List<ProductsModel> GetAllProductsByCategory(string category)
+        {
+
+            var product = (from p in pList
+                           where p.pCategory == category
+                           select p).ToList();
+            return product;
+        }
+
+        public ProductsModel GetProductById(int productId)
+        {
+           
+            
+                var product = (from p in pList
+                               where p.pId == productId
+                               select p).Single();
+                return product;
+           
+           
+        }
+
+        public string AddProduct(ProductsModel newProduct)
+        {
+            pList.Add(newProduct);
+            return "Product Added Successfully";
+        }
+
+        public string DeleteProduct(int productId)
+        {
+
+            var product = (from p in pList
+                           where p.pId == productId
+                           select p).Single();
+            pList.Remove(product);
+            return "Product Removed Successfully";
+        }
+
+        public string UpdateProduct(ProductsModel productupdate)
+        {
+
+            var product = (from p in pList
+                           where p.pId == productupdate.pId
+                           select p).Single();
+
+            product.pName = productupdate.pName;
+            product.pCategory = productupdate.pCategory;
+            product.pPrice = productupdate.pPrice;
+            product.pIsInStock = productupdate.pIsInStock;
+
+            return "Product Updated";
+        }
+
+    }
+}
