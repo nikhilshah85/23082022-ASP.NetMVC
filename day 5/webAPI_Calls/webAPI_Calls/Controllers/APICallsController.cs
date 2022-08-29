883975e5d7dabcb0e757a04f0faf5b3854f26c70
Nikhil <nikhil.shah2008@gmail.com>
2022-08-29 10:20:46 +0530

    Day 5 API Call

---
+++ day 5/webAPI_Calls/webAPI_Calls/Controllers/APICallsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webAPI_Calls.Controllers
+{
+    public class APICallsController : Controller
+    {
+        public IActionResult PostData()
+        {
+            return View();
+        }
+
+        public IActionResult GetAlbums()
+        {
+            return View();
+        }
+    }
+}
