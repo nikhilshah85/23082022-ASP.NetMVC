279346e67cbd8ff1fed84440ca1cf4ce0b491970
Nikhil <nikhil.shah2008@gmail.com>
2022-09-05 11:27:32 +0530

    Day 8

---
+++ day 8/IdentityFramework/IdentityFramework/Controllers/BankingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityFramework.Controllers
+{
+    [Authorize]
+    public class BankingController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult BankingHome()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult DownloadStatement()
+        {
+            return View();
+        }
+        public IActionResult TransferFunds()
+        {
+            return View();
+        }
+    }
+}
---
+++ day 8/bankingAPPMVC/bankingAPPMVC/Controllers/BankingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bankingAPPMVC.Controllers
+{
+    [Authorize]
+    public class BankingController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult BankingHome()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult DownloadStatement()
+        {
+            return View();
+        }
+
+        public IActionResult transferFunds()
+        {
+            return View();
+        }
+    }
+}
